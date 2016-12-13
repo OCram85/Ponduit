21c23fff1cc4b3832aa12c0a7cf476dffe0ae1b8
OCram85 <marco.blessing@googlemail.com>
2016-12-13 14:31:36 +0100

    fix outut error

--- src/Classes/ClassLoader.ps1
+++ src/Classes/ClassLoader.ps1
@@ -1,6 +1,5 @@
 If ($PSVersionTable.PSVersion.Major -ge 5) {
     Write-Verbose "ClassLoader: Importing Classes..." -Verbose
-    Write-Information
     $ClassPath = Join-Path -Path $PSScriptRoot -ChildPath "*.ps1"
     $Items = Get-ChildItem -Path $ClassPath -Recurse -Exclude 'ClassLoader.ps1'
     ForEach ($Item in $Items) {
