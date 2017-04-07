67f54b3a0858aeb8b99fc9bd97b35803c6bf14eb
OCram85 <marco.blessing@googlemail.com>
2017-04-07 13:39:26 +0200

    remove verbose message while importing classes

--- src/Classes/ClassLoader.ps1
+++ src/Classes/ClassLoader.ps1
@@ -1,9 +1,9 @@
 If ($PSVersionTable.PSVersion.Major -ge 5) {
-    Write-Verbose "ClassLoader: Importing Classes..." -Verbose
+    Write-Verbose "ClassLoader: Importing Classes..."
     $ClassPath = Join-Path -Path $PSScriptRoot -ChildPath "*.ps1"
     $Items = Get-ChildItem -Path $ClassPath -Recurse -Exclude 'ClassLoader.ps1'
     ForEach ($Item in $Items) {
-        Write-Verbose $Item.BaseName -Verbose
+        Write-Verbose $Item.BaseName
         . $Item.Fullname
     }
 }
