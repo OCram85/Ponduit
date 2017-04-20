9cee442f524d4397e0300ab05bc7028ab431c76f
OCram85 <marco.blessing@googlemail.com>
2017-04-20 16:35:29 +0200

    adds coverage report verbose build message

--- tools/AppVeyor.psm1
+++ tools/AppVeyor.psm1
@@ -119,6 +119,8 @@ Function Invoke-CoverageReport() {
 
     $FileMap = New-PesterFileMap -SourceRoot '.\src' -PesterRoot '.\tests'
     $CoverageReport = New-CoverageReport -PesterFileMap $FileMap -RepoToken $RepoToken
+    Write-Host "CoverageReport JSON:" -ForegroundColor Yellow
+    $CoverageReport
     Publish-CoverageReport -CoverageReport $CoverageReport
 }
 
