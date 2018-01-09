e5d2a898a366d7eac4fddb851117c1603bd1ed61
OCram85 <marco.blessing@googlemail.com>
2018-01-09 10:06:12 +0100

    move classes to module script

--- src/Classes/ClassLoader.ps1
+++
@@ -1,9 +0,0 @@
-If ($PSVersionTable.PSVersion.Major -ge 5) {
-    Write-Verbose "ClassLoader: Importing Classes..."
-    $ClassPath = Join-Path -Path $PSScriptRoot -ChildPath "*.ps1"
-    $Items = Get-ChildItem -Path $ClassPath -Recurse -Exclude 'ClassLoader.ps1'
-    ForEach ($Item in $Items) {
-        Write-Verbose $Item.BaseName
-        . $Item.Fullname
-    }
-}
